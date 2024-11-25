5fb12aaae1cdee073050b8ce51832410bdee8501
Anwer <anwer@darkhorsesolutions.co>
2024-11-25 15:15:17 +0500

    Add calendar.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,5 +9,35 @@ $(document).ready(function() {
     }
 
     truncateText('.count_events', 55);
-    truncateText('.count_announcements', 60);
-});  
+    truncateText('.count_announcements', 60); 
+
+    // ======== Calendar 
+
+    var $calendar;
+
+    let container = $("#my_calendar").simpleCalendar({
+        fixedStartDay: 0, // begin weeks by sunday
+        disableEmptyDetails: true,
+        events: [
+            // generate new event after tomorrow for one hour
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
+                summary: '"Jahsn-E-Azaadi" - 77th Independence Day of Pakistan.'
+            },
+            // generate new event for yesterday at noon
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
+                summary: 'Annual Picnic'
+            },
+            // generate new event for the last two days
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
+                summary: 'Visit of the Lahore office'
+            }
+        ],
+    });
+    $calendar = container.data('plugin_simpleCalendar')
+}); 
