10cbae021965d352781dd97c5a24a28a3f512a48
Anwer <anwer@darkhorsesolutions.co>
2024-12-09 15:54:44 +0500

    Update chatbot.

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,70 +1,11 @@
-const chatButton = document.getElementById("chat-button");
-const chatContainer = document.getElementById("chatContainer");
-const minimizeButton = document.getElementById("minimize-button");
-const chatInput = document.getElementById("chat-input");
-const chatMessages = document.getElementById("conversation-group");
-const sendButton = document.getElementById("SentButton");
-
-if (chatButton) {
-    chatButton.addEventListener("click", function () {
-        if (chatContainer) {
-            chatContainer.classList.add("open");
-            chatButton.classList.add("clicked");
-        }
-    });
-}
-
-if (minimizeButton) {
-    minimizeButton.addEventListener("click", function () {
-        if (chatContainer) {
-            chatContainer.classList.remove("open");
-            chatButton.classList.remove("clicked");
-        }
-    });
-}
-
-function createMessage(message, isUser = true) {
-    const newMessage = document.createElement('div');
-    newMessage.classList.add(isUser ? 'sentText' : 'botText');
-    newMessage.textContent = message;
-    chatMessages.appendChild(newMessage);
-    return newMessage;
-}
-
-function chatbotResponse() {
-    const messages = ["Hello!", "How can I assist you?", "Let me know if you have any further questions"];
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    const message = messages[randomIndex];
-    const botMessage = createMessage(message, false);
-    botMessage.scrollIntoView();
-}
-
-chatInput.addEventListener("input", function (event) {
-    if (event.target.value) {
-        sendButton.classList.add("svgsent");
-    } else {
-        sendButton.classList.remove("svgsent");
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+      window.voiceflow.chat.load({
+        verify: { projectID: '672a6273e7c18363c7364501' },
+        url: 'https://general-runtime.voiceflow.com',
+        versionID: 'production'
+      });
     }
-});
-
-chatInput.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13) {
-        const message = chatInput.value;
-        chatInput.value = "";
-        const userMessage = createMessage(message);
-        userMessage.scrollIntoView();
-        setTimeout(chatbotResponse, 1000);
-        sendButton.classList.add("svgsent");
-    }
-});
-
-if (sendButton) {
-    sendButton.addEventListener("click", function () {
-        const message = chatInput.value;
-        chatInput.value = "";
-        const userMessage = createMessage(message);
-        userMessage.scrollIntoView();
-        setTimeout(chatbotResponse, 1000);
-        sendButton.classList.add("svgsent");
-    });
-}
+    v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+})(document, 'script');
